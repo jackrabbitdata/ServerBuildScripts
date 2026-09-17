803d89747ea21e883490a5e06c7d65b7c4185457
Patrick Kehn <kehnpatrick@gmail.com>
2026-09-16 22:03:31 -0500

    Update newDebianLAMP.sh

--- newDebianLAMP.sh
+++ newDebianLAMP.sh
@@ -299,7 +299,7 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
-    sudo tee /etc/apt/sources.list.d/docker.sources <<-EOF
+    sudo tee /etc/apt/sources.list.d/docker.sources <<- EOF
     Types: deb
     URIs: https://download.docker.com/linux/debian
     Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
